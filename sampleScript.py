e458a8eec7a5a1f1caeb0192ac84588d5cea9f39
Mmasehume <Mmasehumebbd@gmail.com>
2024-04-15 15:13:15 +0200

    update sample script

--- sampleScript.py
+++ sampleScript.py
@@ -1,8 +1,5 @@
 from uuid import uuid4
-from config import config, getConfig
-from index import pay, userInfo, paymentInfo
-from datetime import datetime, timedelta
-
+from index import config, getConfig, pay, userInfo, paymentInfo
 
 if __name__ == "__main__":
     config("2020122653946739963336", "216620000000188034591", "./certificates/rsa_private_key.pem", "sandbox")
@@ -10,13 +7,12 @@ if __name__ == "__main__":
     # print(getConfig())
 
     # user info
-    # authcode = "0000000001aSwCRUNfce76y800675232"
+    # authcode = "0000000001sUUMM2meQy767N00719609"
     # userInfoResponse = userInfo(authcode)
     # print(userInfoResponse)
 
     # payment
-    # now = datetime.now() + timedelta(hours=2)
-    # paymentExpiryTime = now.astimezone().isoformat()
+    # paymentExpiryTime = 2
     # paymentId = uuid4()
     # paymentNotifyUrl = "https://webhook.site/c7add433-3e51-4e46-8ebc-9adb4a129042"
     # amountInCents = 2000
@@ -25,6 +21,6 @@ if __name__ == "__main__":
     # paymentResponse = pay(paymentId, paymentNotifyUrl, paymentExpiryTime, amountInCents, goodsId, buyerId)
     # print(paymentResponse)
 
-    # # payment Info
-    # paymentInfoResponse = paymentInfo('20240213111212800100166768100558564')
+    # payment Info
+    # paymentInfoResponse = paymentInfo('20240415111212800100166760400601553')
     # print(paymentInfoResponse)
